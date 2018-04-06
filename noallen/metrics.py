2931e4a7c9b877fac9eed1d5784f25b3fe3e5bf1
omerlevy <>
2018-04-06 16:04:58 -0700

    cosmetics

--- noallen/metrics.py
+++ noallen/metrics.py
@@ -1,7 +1,20 @@
-from typing import Optional
 import torch
 import numpy as np
 from torch.autograd import Variable
+
+
+def mrr(predictions, gold_labels, all_true, candidates=None):
+    reciprocal_ranks = []
+    candidate_mask = get_mask(all_true, candidates, gold_labels, predictions.size(1))
+    predictions = torch.sigmoid(predictions)
+    predictions = predictions * candidate_mask
+    max_values, argsort = torch.sort(predictions, 1, descending=True)
+    argsort = argsort.data.cpu().numpy()
+    gold_labels = gold_labels.data.cpu().numpy()
+    for i in range(predictions.size(0)):
+        rank = np.where(argsort[i] == gold_labels[i])[0][0]
+        reciprocal_ranks.append(rank + 1)
+    return reciprocal_ranks
 
 
 def masked_index_fill(tensor, index, index_mask, value):
@@ -26,18 +39,3 @@ def get_mask(all_true_objects, candidates, gold_labels, num_labels):
     candidates_mask.scatter_(1, gold_labels.unsqueeze(1).data, 1)
     return Variable(candidates_mask.float(), requires_grad=True)
 
-def mrr(predictions: torch.Tensor,
-                 gold_labels: torch.Tensor,
-                 all_true: torch.Tensor,
-                 candidates: Optional[torch.Tensor]=None):
-    reciprocal_ranks = []
-    candidate_mask = get_mask(all_true, candidates, gold_labels, predictions.size(1))
-    predictions = torch.sigmoid(predictions)
-    predictions = predictions * candidate_mask
-    max_values, argsort = torch.sort(predictions, 1, descending=True)
-    argsort = argsort.data.cpu().numpy()
-    gold_labels = gold_labels.data.cpu().numpy()
-    for i in range(predictions.size(0)):
-        rank = np.where(argsort[i] == gold_labels[i])[0][0]
-        reciprocal_ranks.append(rank + 1)
-    return reciprocal_ranks
