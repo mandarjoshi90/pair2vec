1b6cd06d48c9d82521a0c3de7fec55e5d4923806
Mandar Joshi <mandar90@cs.washington.edu>
2018-04-17 15:33:39 -0700

    fix padding bug

--- noallen/model.py
+++ noallen/model.py
@@ -36,9 +36,10 @@ class RelationalEmbeddingModel(Module):
         self.init()
     
     def to_tensors(self, fields):
-        return ((field['tokens'], get_text_field_mask(field)) if isinstance(field, Dict) else (field, torch.eq(field, self.pad)) for field in fields)
+        return ((field['tokens'], get_text_field_mask(field)) if isinstance(field, Dict) else (field, 1.0 - torch.eq(field, self.pad)) for field in fields)
     
     def init(self):
+        [xavier_normal(p) for p in self.parameters() if len(p.size()) > 1]
         if isinstance(self.represent_arguments, Embedding):
             self.represent_arguments.weight.data.copy_(self.arg_vocab.vectors())
             #pass #retrained_embeddings_or_xavier(self.config, self.represent_arguments, self.vocab, self.config.argument_namespace)
