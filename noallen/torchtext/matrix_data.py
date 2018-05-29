d88784c62dfd0dcc4229bf710172f166f7d0f46c
Mandar Joshi <mandar90@cs.washington.edu>
2018-05-28 18:20:08 -0700

    fix shuffle bug

--- noallen/torchtext/matrix_data.py
+++ noallen/torchtext/matrix_data.py
@@ -25,25 +25,63 @@ def smoothed_sampling(instances, alpha=None):
     sample = np.take(unique, sample_idxs, axis=0)
     return sample
 
+def uniform_type_sampling(instances, scores_matrix, indxs_matrix):
+    # (num_ins, topk)
+    batch_indxs = np.take(indxs_matrix, instances, axis=0)
+    # (num_ins, 1))
+    sample_idx_idxs = np.random.randint(0, indxs_matrix.shape[1], indxs_matrix.shape[0])
+    # import ipdb
+    # ipdb.set_trace()
+    # (num_ins, 1))
+    sample_idxs = np.take(batch_indxs, sample_idx_idxs, axis=1)
+    return sample_idxs
+def batched_unigram_type_sampling(instances, scores_matrix, indxs_matrix):
+    # (num_ins, topk)
+    batch_indxs = np.take(indxs_matrix, instances, axis=0)
+    # (num_ins, topk)
+    batch_scores = np.take(scores_matrix, instances, axis=0)
+    # (num_ins, 1))
+    sample_idx_idxs = torch.multinomial(torch.from_numpy(batch_scores), 1, replacement=True)
+    # import ipdb
+    # ipdb.set_trace()
+    # (num_ins, 1))
+    sample_idxs = np.take(batch_indxs, sample_idx_idxs.cpu().numpy(), axis=1)
+    return sample_idxs
+
+def unigram_type_sampling(instances, scores_matrix, indxs_matrix, batch_size=10000):
+    samples = []
+    for i in range(0, instances.shape[0], batch_size):
+        samples.append(batched_unigram_type_sampling(instances[i: i + batch_size], scores_matrix, indxs_matrix))
+    return np.concatenate(samples)
+
+
 def shuffled_sampling(instances):
     return np.random.permutation(instances)
 
-def sample(instances, alpha=None):
-    if alpha is None:
-        np.random.shuffle(instances)
+def sample(instances, alpha=None, compositional_rels=True, type_scores=None, type_indices=None):
+    # if alpha is None:
+    np.random.shuffle(instances)
     subjects, objects, relations = instances[:, 0],  instances[:, 1],  instances[:, 2:]
+    relations = relations if compositional_rels or relations.shape[1] > 1 else relations.reshape(relations.shape[0])
     sample_fn, kwargs = (smoothed_sampling, {'alpha': alpha}) if alpha is not None else (shuffled_sampling, {})
     sampled_subjects, sampled_objects, sampled_relations = sample_fn(subjects, **kwargs), sample_fn(objects, **kwargs), sample_fn(relations, **kwargs)
-    return  subjects, objects, relations, sampled_relations, sampled_subjects, sampled_objects
+    type_sampled_subjects, type_sampled_objects = None, None
+    # if type_scores is not None:
+        # type_sampled_subjects = uniform_type_sampling(subjects, type_scores, type_indices)
+        # type_sampled_objects = uniform_type_sampling(objects, type_scores, type_indices)
+    return  subjects, objects, relations, sampled_relations, sampled_subjects, sampled_objects #, type_sampled_subjects, type_sampled_objects
 
 
 
 class TripletIterator():
-    def __init__(self, batch_size, fields, return_nl=False, limit=None):
+    def __init__(self, batch_size, fields, return_nl=False, limit=None, compositional_rels=True, type_scores_file=None, type_indices_file=None):
         self.batch_size = batch_size
         self.fields = fields
         self.return_nl = return_nl
         self.limit = limit
+        self.compositional_rels = compositional_rels
+        self.type_scores = None if type_scores_file is None else np.load(type_scores_file)
+        self.type_indices = None if type_indices_file is None else np.load(type_indices_file)
 
     def __call__(self, data, device=-1, train=True):
         batches = self._create_batches(data, device, train)
@@ -53,18 +91,16 @@ class TripletIterator():
 
     def _create_batches(self, instance_gen, device=-1, train=True):
         for instances in instance_gen:
-            #if self.limit is not None:
-            #    instances = instances[:self.limit]
             start = 0
+            # instances = instances[:500]
             #inputs = subjects, objects, relations, sampled_relations, sampled_subjects, sampled_objects
-            inputs = instances if not train else sample(instances, 0.75)
-            effective_vocab_sizes = [len(f.vocab) - len(f.vocab.specials) for f in self.fields]
-            #inputs = tuple((ip%efs) + len(self.fields[i].vocab.specials) for i, (ip, efs) in enumerate(zip(inputs, effective_vocab_sizes)))
-            #while True:
+            inputs = instances if not train else sample(instances, 0.75, self.compositional_rels, self.type_scores, self.type_indices)
+            # import ipdb
+            # ipdb.set_trace()
             for num, batch_start in enumerate(range(0, inputs[0].shape[0], self.batch_size)):
                 tensors = tuple(Variable(torch.LongTensor(x[batch_start: batch_start + self.batch_size]), requires_grad=False) for x in inputs)
                 if device == None:
-                    tensors = tuple([t.cuda() for t in tensors])
+                    tensors = tuple([t.cuda() if t is not None else None for t in tensors])
                 if self.return_nl:
                     subject_nl = [self.fields[0].vocab.itos[i] for i in inputs[0][batch_start: batch_start + self.batch_size]]
                     object_nl = [self.fields[1].vocab.itos[i] for i in inputs[1][batch_start: batch_start + self.batch_size]]
@@ -86,7 +122,7 @@ def create_vocab(config, field):
         text = f.read()
         tokens = text.rstrip().split('\n')
     # specials = list(OrderedDict.fromkeys(tok for tok in [field.unk_token, field.pad_token, field.init_token, field.eos_token] if tok is not None))
-    specials = ['<unk>', '<pad>', '<X>', '<Y>']
+    specials = ['<unk>', '<pad>', '<X>', '<Y>'] if config.compositional_rels else ['<unk>', '']
     #vocab = Vocab(tokens, specials=specials, vectors='glove.6B.300d', vectors_cache='/glove')
     vocab = Vocab(tokens, specials=specials, vectors='fasttext.en.300d', vectors_cache='/fasttext')
     #vocab = Vocab(tokens, specials=specials)
@@ -99,11 +135,13 @@ def read(filenames):
             logger.info('Loading {} instances from {}'.format(instances.shape[0], fname))
             yield instances
 
-def read_dev(fname, limit=None):
+def read_dev(fname, limit=None, compositional_rels=True, type_scores_file=None, type_indices_file=None):
     instances = np.load(fname)
     instances = instances[:limit] if limit is not None else instances
     logger.info('Loading {} instances from {}'.format(instances.shape[0], fname))
-    return sample(instances)
+    type_scores = None if type_scores_file is None else np.load(type_scores_file)
+    type_indices = None if type_indices_file is None else np.load(type_indices_file)
+    return sample(instances, alpha=.75, compositional_rels=compositional_rels, type_scores=type_scores, type_indices=type_indices)
 
 def dev_data(sample):
     yield sample
@@ -113,7 +151,9 @@ def create_dataset(config):
     #files = [os.path.join(config.triplet_dir, fname) for fname in os.listdir(config.triplet_dir) if fname.endswith('.npy')]
     files = [os.path.join(config.triplet_dir, 'triplets_' + str(i) + '.npy') for i in range(1, 1000)]
     train_data = _LazyInstances(lambda : iter(read(files[1:])))
-    validation_sample = read_dev(files[0], 500000)
+    type_scores_file = config.type_scores_file if hasattr(config, 'type_scores_file') else None
+    type_indices_file = config.type_indices_file if hasattr(config, 'type_indices_file') else None
+    validation_sample = read_dev(files[0], 500000, config.compositional_rels, type_scores_file, type_indices_file)
     validation_data = _LazyInstances(lambda : iter (dev_data(validation_sample)))
     return train_data, validation_data
 
@@ -128,8 +168,11 @@ def read_data(config, return_nl=False, preindex=True):
     config.n_rels = len(rels.vocab)
     sample_arguments = hasattr(config, "sample_arguments") and config.sample_arguments
     fields = fields + [rels, args, args] if sample_arguments else fields  + [rels]
+    type_scores_file = config.type_scores_file if hasattr(config, 'type_scores_file') else None
+    type_indices_file = config.type_indices_file if hasattr(config, 'type_indices_file') else None
 
-    train_iterator = TripletIterator(config.train_batch_size, fields , return_nl=return_nl)
-    dev_iterator = TripletIterator(config.dev_batch_size, fields, return_nl=return_nl)
+    train_iterator = TripletIterator(config.train_batch_size, fields , return_nl=return_nl, 
+            compositional_rels=config.compositional_rels, type_scores_file=type_scores_file, type_indices_file=type_indices_file)
+    dev_iterator = TripletIterator(config.dev_batch_size, fields, return_nl=return_nl, compositional_rels=config.compositional_rels)
 
     return train, dev, train_iterator, dev_iterator, args, rels
