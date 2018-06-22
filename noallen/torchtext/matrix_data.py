608ab201e07aebc29b86e087faa80d015555b44d
Mandar Joshi <mandar90@cs.washington.edu>
2018-06-22 14:41:33 -0700

    smoothing via config

--- noallen/torchtext/matrix_data.py
+++ noallen/torchtext/matrix_data.py
@@ -58,37 +58,37 @@ def unigram_type_sampling(instances, scores_matrix, indxs_matrix, batch_size=100
 def shuffled_sampling(instances):
     return np.random.permutation(instances)
 
-# def sample_compositional(instances, alpha=None, compositional_rels=True, type_scores=None, type_indices=None, num_neg_samples=1):
-    # # if alpha is None:
-    # np.random.shuffle(instances)
-    # subjects, objects, relations = instances[:, 0],  instances[:, 1],  instances[:, 2:]
-    # relations = relations if compositional_rels or relations.shape[1] > 1 else relations.reshape(relations.shape[0])
-    # sample_fn, kwargs = (smoothed_sampling, {'alpha': alpha}) if alpha is not None else (shuffled_sampling, {})
-    # sampled_relations = sample_fn(relations, **kwargs)
-    # sample_fn, kwargs = (smoothed_sampling, {'alpha': alpha, 'num_neg_samples': num_neg_samples}) if alpha is not None else (shuffled_sampling, {})
-    # sampled_subjects, sampled_objects  = sample_fn(subjects, **kwargs).reshape((instances.shape[0], num_neg_samples)), sample_fn(objects, **kwargs).reshape((instances.shape[0], num_neg_samples))
-    # type_sampled_subjects, type_sampled_objects = None, None
-    # # if type_scores is not None:
-        # # type_sampled_subjects = uniform_type_sampling(subjects, type_scores, type_indices)
-        # # type_sampled_objects = uniform_type_sampling(objects, type_scores, type_indices)
-    # return  subjects, objects, relations, sampled_relations, sampled_subjects, sampled_objects #, type_sampled_subjects, type_sampled_objects
-
-
 def sample_compositional(instances, alpha=None, compositional_rels=True, type_scores=None, type_indices=None, num_neg_samples=1):
     # if alpha is None:
     np.random.shuffle(instances)
-    sampled_subjects, sampled_objects, subjects, objects, relations = instances[:, 0],  instances[:, 1], instances[:, 2], instances[:, 3], instances[:, 4:]
-    sampled_subjects, sampled_objects = sampled_subjects.reshape((instances.shape[0], num_neg_samples)), sampled_objects.reshape((instances.shape[0], num_neg_samples))
+    subjects, objects, relations = instances[:, 0],  instances[:, 1],  instances[:, 2:]
     relations = relations if compositional_rels or relations.shape[1] > 1 else relations.reshape(relations.shape[0])
     sample_fn, kwargs = (smoothed_sampling, {'alpha': alpha}) if alpha is not None else (shuffled_sampling, {})
     sampled_relations = sample_fn(relations, **kwargs)
-    # sample_fn, kwargs = (smoothed_sampling, {'alpha': alpha, 'num_neg_samples': num_neg_samples}) if alpha is not None else (shuffled_sampling, {})
-    # sampled_subjects, sampled_objects  = sample_fn(subjects, **kwargs).reshape((instances.shape[0], num_neg_samples)), sample_fn(objects, **kwargs).reshape((instances.shape[0], num_neg_samples))
+    sample_fn, kwargs = (smoothed_sampling, {'alpha': alpha, 'num_neg_samples': num_neg_samples}) if alpha is not None else (shuffled_sampling, {})
+    sampled_subjects, sampled_objects  = sample_fn(subjects, **kwargs).reshape((instances.shape[0], num_neg_samples)), sample_fn(objects, **kwargs).reshape((instances.shape[0], num_neg_samples))
     type_sampled_subjects, type_sampled_objects = None, None
     # if type_scores is not None:
         # type_sampled_subjects = uniform_type_sampling(subjects, type_scores, type_indices)
         # type_sampled_objects = uniform_type_sampling(objects, type_scores, type_indices)
     return  subjects, objects, relations, sampled_relations, sampled_subjects, sampled_objects #, type_sampled_subjects, type_sampled_objects
+
+
+# def sample_compositional(instances, alpha=None, compositional_rels=True, type_scores=None, type_indices=None, num_neg_samples=1):
+    # # if alpha is None:
+    # np.random.shuffle(instances)
+    # sampled_subjects, sampled_objects, subjects, objects, relations = instances[:, 0],  instances[:, 1], instances[:, 2], instances[:, 3], instances[:, 4:]
+    # sampled_subjects, sampled_objects = sampled_subjects.reshape((instances.shape[0], num_neg_samples)), sampled_objects.reshape((instances.shape[0], num_neg_samples))
+    # relations = relations if compositional_rels or relations.shape[1] > 1 else relations.reshape(relations.shape[0])
+    # sample_fn, kwargs = (smoothed_sampling, {'alpha': alpha}) if alpha is not None else (shuffled_sampling, {})
+    # sampled_relations = sample_fn(relations, **kwargs)
+    # # sample_fn, kwargs = (smoothed_sampling, {'alpha': alpha, 'num_neg_samples': num_neg_samples}) if alpha is not None else (shuffled_sampling, {})
+    # # sampled_subjects, sampled_objects  = sample_fn(subjects, **kwargs).reshape((instances.shape[0], num_neg_samples)), sample_fn(objects, **kwargs).reshape((instances.shape[0], num_neg_samples))
+    # type_sampled_subjects, type_sampled_objects = None, None
+    # # if type_scores is not None:
+        # # type_sampled_subjects = uniform_type_sampling(subjects, type_scores, type_indices)
+        # # type_sampled_objects = uniform_type_sampling(objects, type_scores, type_indices)
+    # return  subjects, objects, relations, sampled_relations, sampled_subjects, sampled_objects #, type_sampled_subjects, type_sampled_objects
 
 def sample_pairs(instances, alpha=None, compositional_rels=True, type_scores=None, type_indices=None, num_neg_samples=1):
     # if alpha is None:
@@ -103,12 +103,14 @@ def sample_pairs(instances, alpha=None, compositional_rels=True, type_scores=Non
     return pairs, relations, sampled_relations
 
 class TripletIterator():
-    def __init__(self, batch_size, fields, pairwise=False, return_nl=False, limit=None, compositional_rels=True, type_scores_file=None, type_indices_file=None, num_neg_samples=1):
+    def __init__(self, batch_size, fields, pairwise=False, return_nl=False, limit=None, compositional_rels=True, type_scores_file=None, type_indices_file=None, num_neg_samples=1,
+            alpha=0.75):
         self.batch_size = batch_size
         self.pairwise = pairwise
         self.fields = fields
         self.return_nl = return_nl
         self.limit = limit
+        self.alpha = alpha
         self.compositional_rels = compositional_rels
         self.num_neg_samples = num_neg_samples
         self.type_scores = None if type_scores_file is None else np.load(type_scores_file)
@@ -126,7 +128,7 @@ class TripletIterator():
             # instances = instances[:500]
             #inputs = subjects, objects, relations, sampled_relations, sampled_subjects, sampled_objects
             sample = sample_pairs if self.pairwise else sample_compositional
-            inputs = instances if not train else sample(instances, 0.75, self.compositional_rels, self.type_scores, self.type_indices, self.num_neg_samples)
+            inputs = instances if not train else sample(instances, self.alpha, self.compositional_rels, self.type_scores, self.type_indices, self.num_neg_samples)
             # import ipdb
             # ipdb.set_trace()
             for num, batch_start in enumerate(range(0, inputs[0].shape[0], self.batch_size)):
@@ -205,7 +207,9 @@ def read_data(config, return_nl=False, preindex=True):
     type_indices_file = config.type_indices_file if hasattr(config, 'type_indices_file') else None
 
     train_iterator = TripletIterator(config.train_batch_size, fields , return_nl=return_nl, 
-            compositional_rels=config.compositional_rels, type_scores_file=type_scores_file, type_indices_file=type_indices_file, pairwise=config.pairwise, num_neg_samples=config.num_neg_samples)
-    dev_iterator = TripletIterator(config.dev_batch_size, fields, return_nl=return_nl, compositional_rels=config.compositional_rels, pairwise=config.pairwise, num_neg_samples=config.num_neg_samples)
+            compositional_rels=config.compositional_rels, type_scores_file=type_scores_file, type_indices_file=type_indices_file, pairwise=config.pairwise, num_neg_samples=config.num_neg_samples,
+            alpha=getattr(config, 'alpha', 0.75))
+    dev_iterator = TripletIterator(config.dev_batch_size, fields, return_nl=return_nl, compositional_rels=config.compositional_rels, pairwise=config.pairwise, num_neg_samples=config.num_neg_samples,
+            alpha=getattr(config, 'alpha', 0.75))
 
     return train, dev, train_iterator, dev_iterator, args, rels
