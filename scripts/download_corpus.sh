3eb506475d6e45498452e9f8139f07bd26f6f263
Mandar Joshi <mandar90@cs.washington.edu>
2019-01-09 01:04:27 -0800

    fix soft link bug

--- scripts/download_corpus.sh
+++ scripts/download_corpus.sh
@@ -13,4 +13,4 @@ mkdir $data_dir/fasttext
 curl -o $data_dir/fasttext/wiki-news-300d-1M-subword.vec.zip https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki-news-300d-1M-subword.vec.zip
 unzip $data_dir/fasttext/wiki-news-300d-1M-subword.vec.zip -d $data_dir/fasttext/
 rm $data_dir/fasttext/wiki-news-300d-1M-subword.vec.zip
-ln -s $data_dir/fasttext/wiki-news-300d-1M-subword.vec wiki.en.vec
+ln -s $data_dir/fasttext/wiki-news-300d-1M-subword.vec $data_dir/fasttext/wiki.en.vec
